f7f9f3bda47dfc407b66916364fbb346ef2ffe93
Your Name <you@example.com>
2019-03-26 21:17:53 -0400

    changed email regex

--- src/scrape/TouroScraper.java
+++ src/scrape/TouroScraper.java
@@ -117,8 +117,8 @@ public class TouroScraper {
 	
 	public static void collectEmails(Document doc) 
 	{
-		//copied regex from emailregex.com
-		Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+		//copied regex from Hirsch G's post on slack after the other one included phone numbers
+		Pattern p = Pattern.compile("\\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9.-]+\\b");
 		try 
 		{
 			Matcher matcher = p.matcher(doc.text());
